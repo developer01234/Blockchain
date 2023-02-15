0d66aa3d6dc00e7802643d437d04aca8ced4e838
Ruslan Shvetsov <ruslansorokin234567@gmail.com>
2023-02-16 00:43:17 +0300

    bugfix: sort block

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
+import hashlib
 import json
 import os
 
 
+def get_hash(filename):
+    blockchain_dir = os.curdir + '/blockchain/'
+    file = open(blockchain_dir + filename, 'rb').read()
+    return hashlib.md5(file).hexdigest()
+
+def check_integrity():
+    blockchain_dir = os.curdir + '/blockchain/'
+    files = os.listdir(blockchain_dir)
+    files = sorted([int(i) for i in files])
+
+    for file in files[1:]:
+        h = json.load(open(blockchain_dir + str(file)))['hash']
+ 
 def create_block(user, amoutCoin, transactionTo, hash=''):
     blockchain_dir = os.curdir + '/blockchain/'
-    files = sorted(os.listdir(blockchain_dir))
+    files = os.listdir(blockchain_dir)
+    files = sorted([int(i) for i in files])
 
-    data = {"user": user,
-            "amoutCoin": amoutCoin,
-            "transactionTo": transactionTo,
-            "hash": hash}
+    last_file = files[-1]
+    filename = str(last_file + 1)
 
-    with open(blockchain_dir + '2', 'w') as file:
+    hash = get_hash(str(last_file))
+
+    data = {'user': user,
+            'amoutCoin': amoutCoin,
+            'transactionTo': transactionTo,
+            'hash': hash}
+
+    with open(blockchain_dir + filename, 'w') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
 def main():
